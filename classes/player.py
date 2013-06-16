30934cb651c720d8d75da4d6ec3683d616f0aa77
Erik Horton <hortonew@gmail.com>
2013-06-15 23:19:04 -0400

    No more collision detection

--- classes/player.py
+++ classes/player.py
@@ -1,28 +1,36 @@
 from pyglet.window import key
 import physicalobject, resources
-from config import PLAYER_THRUST
+from config import PLAYER_THRUST, PLAYER_LIVES
 
 class Player(physicalobject.PhysicalObject):
 
-    def __init__(self, *args, **kwargs):
-    	super(Player, self).__init__(img=resources.player_image, *args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super(Player, self).__init__(img=resources.player_image, *args, **kwargs)
 
-        self.thrust = PLAYER_THRUST
+		self.thrust = PLAYER_THRUST
+		self.lives = PLAYER_LIVES
 
-        self.keys = dict(left=False, right=False, up=False, down=False)
+		self.keys = dict(left=False, right=False, up=False, down=False)
 
-        # Place for key mappings
-        self.up = key.W
-        self.left = key.A
-        self.right = key.D
-        self.down = key.S
+		# Place for key mappings
+		self.up = key.W
+		self.left = key.A
+		self.right = key.D
+		self.down = key.S
 
-        self.key_handler = key.KeyStateHandler()
+		self.key_handler = key.KeyStateHandler()
 
-    def delete(self):
-    	super(Player, self).delete()
+	def delete(self):
+		super(Player, self).delete()
 
-    def update(self, dt):
+	def respawn(self):
+		self.lives -= 1
+		self.x = 400
+		self.y = 300
+		self.dead = False
+		self.velocity_x, self.velocity_y = 0.0, 0.0
+
+	def update(self, dt):
 		super(Player, self).update(dt)
 
 		if self.key_handler[self.up]:
--- main.py
+++ main.py
@@ -10,9 +10,7 @@ main_batch = pyglet.graphics.Batch()
 player_ship = player.Player(x=400, y=300, batch=main_batch)
 
 eg = EnemyGroup(NUM_ENEMIES, main_batch)
-#en1 = Enemy(x=500, y=700, batch=main_batch)
 game_objects = [player_ship, eg]
-
 
 # Tell the main window that the player object responds to events
 game_window.push_handlers(player_ship.key_handler)
@@ -21,33 +19,14 @@ game_window.push_handlers(player_ship.key_handler)
 @game_window.event
 def on_draw():
 	game_window.clear()
-
 	main_batch.draw()
 
 
 def update(dt):
-	# collision detection
-	all_enemies = eg.enemies
-	for en in eg.enemies:
-		obj_1 = player_ship
-		obj_2 = en
-
-		if not obj_1.dead and not obj_2.dead:
-			if obj_1.collides_with(obj_2):
-				obj_1.handle_collision_with(obj_2)
-				obj_2.handle_collision_with(obj_1)
-
-	# remove objects from game_objects and delete them
-	if player_ship.dead:
-		player_ship.delete()
-		game_objects.remove(player_ship)
-	for obj in eg.enemies:
-		if obj.dead:
-			eg.remove(obj)
-
 	# update all game objects
 	for obj in game_objects:
 		obj.update(dt)
+	
 
 if __name__ == "__main__":
 	pyglet.clock.schedule_interval(update, 1/120.0)
